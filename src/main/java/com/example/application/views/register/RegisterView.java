8ceb8426324e3646651807090a335f6286a0ffa2
Orestis Papadopoulos <o.papadopoulos@acg.edu>
2023-02-26 05:07:04 +0200

    Can now display QR code image dynamically - the image is not stored.

--- src/main/java/com/example/application/views/register/RegisterView.java
+++ src/main/java/com/example/application/views/register/RegisterView.java
@@ -11,7 +11,6 @@ import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.button.ButtonVariant;
 import com.vaadin.flow.component.formlayout.FormLayout;
 import com.vaadin.flow.component.html.Image;
-import com.vaadin.flow.component.html.Input;
 import com.vaadin.flow.component.html.Span;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
@@ -19,10 +18,13 @@ import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.data.binder.Binder;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
+import com.vaadin.flow.server.InputStreamFactory;
+import com.vaadin.flow.server.StreamResource;
 
-import javax.lang.model.element.Element;
-import java.io.File;
-import java.io.IOException;
+import javax.imageio.ImageIO;
+import javax.xml.transform.stream.StreamSource;
+import java.awt.image.BufferedImage;
+import java.io.*;
 
 @PageTitle("Register")
 @Route(value = "register", layout = MainLayout.class)
@@ -34,16 +36,15 @@ public class RegisterView extends VerticalLayout {
     VerticalLayout tipsLayout = new VerticalLayout();
     HorizontalLayout scanLayout = new HorizontalLayout();
     VerticalLayout qrLayout = new VerticalLayout();
-    Image qr_code = new Image("images/qr_data.png", "Missing");
+    Image qr_code = new Image();
 
     // for data binding
     Binder<User> binder = new Binder<>(User.class);
     User user = new User();
-
-    // for QR code generation
     String qrcode_data = user.getUuid() + "";
-    String path = "C:\\Users\\Orestis\\IntelliJ Projects\\my-app-4\\src\\main\\resources\\META-INF\\resources\\images\\qr_data.png";
     String charset = "UTF-8";
+    ByteArrayOutputStream imageBuffer = null;
+
 
     public RegisterView() throws IOException, WriterException {
         // automatic binding
@@ -62,14 +63,41 @@ public class RegisterView extends VerticalLayout {
             createNewUser();
         });
 
-        //generateQRCode(qrcode_data, path, charset, 300, 300);
-
         Span tip1 = new Span("The QR code to the right is your password. Open your mobile app to scan it.");
         Span tip2 = new Span("After you've scanned it, scan your card over your phone to complete the registration.");
         Button btn_cancel = new Button("Cancel");
         btn_cancel.addThemeVariants(ButtonVariant.LUMO_ERROR);
         tipsLayout.add(tip1, tip2, btn_cancel);
 
+        // for "Dynamic Content" from: https://vaadin.com/docs/latest/advanced/dynamic-content#using-streamresource
+        // found the constructor here: https://github.com/vaadin/flow/blob/main/flow-server/src/main/java/com/vaadin/flow/server/StreamResource.java
+        // once you type InputStreamFactory in, it prompts for the methods to implement
+        // goto vaadin book p. 91 and adjust the implementation of getStream() to create dynamic image
+        StreamResource resource = new StreamResource("", new InputStreamFactory() {
+            @Override
+            public InputStream createInputStream() {
+                BitMatrix matrix = null;
+                try {
+                    matrix = new MultiFormatWriter().encode(new String(qrcode_data.getBytes(charset), charset), BarcodeFormat.QR_CODE, 300, 300);
+                } catch (WriterException | UnsupportedEncodingException e) {
+                    throw new RuntimeException(e);
+                }
+                BufferedImage image = MatrixToImageWriter.toBufferedImage(matrix);
+                imageBuffer = new ByteArrayOutputStream();
+                try {
+                    ImageIO.write(image, "png", imageBuffer);
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+                return new ByteArrayInputStream(imageBuffer.toByteArray());
+            }
+
+            @Override
+            public boolean requiresLock() {
+                return InputStreamFactory.super.requiresLock();
+            }
+        });
+        qr_code = new Image(resource, "QR code resource missing: failed to load dynamically");
         qrLayout.add(qr_code);
 
         scanLayout.add(tipsLayout, qrLayout);
@@ -89,10 +117,5 @@ public class RegisterView extends VerticalLayout {
 //        } catch (ValidationException e) {
 //            throw new RuntimeException(e);
 //        }
-    }
-
-    public static void generateQRCode(String data, String path, String charset, int h, int w) throws WriterException, IOException {
-        BitMatrix matrix = new MultiFormatWriter().encode(new String(data.getBytes(charset), charset), BarcodeFormat.QR_CODE, w, h);
-        MatrixToImageWriter.writeToFile(matrix, path.substring(path.lastIndexOf('.') + 1), new File(path));
     }
 }
