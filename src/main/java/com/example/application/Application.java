bed017cc8f9d4a33cb519feee1ad6f42a8f39428
Orestis Papadopoulos <o.papadopoulos@acg.edu>
2023-04-23 19:11:56 +0300

    jar executable built successfully.

--- src/main/java/com/example/application/Application.java
+++ src/main/java/com/example/application/Application.java
@@ -6,6 +6,8 @@ import com.vaadin.flow.component.page.Push;
 import com.vaadin.flow.theme.Theme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 /**
@@ -23,7 +25,12 @@ import org.springframework.scheduling.annotation.EnableAsync;
 // at first, I added this annotation in MainLayout class; an error message said that it has to go to a class which implements AppShellConfigurator
 // see: "Asynchronous Updates", and "Enabling Server Push"
 @Push
-public class Application implements AppShellConfigurator {
+public class Application extends SpringBootServletInitializer implements AppShellConfigurator {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
 
     public static void main(String[] args) {
 
